5f694f1642159649ae18154bfc09de79ba069a78
Gustavo garcia <Garciamontielgustavo@gmail.com>
2024-06-24 14:40:43 -0500

    Registrar

--- src/licorera/Main.java
+++ src/licorera/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Views/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Views/Register.fxml"));
         
         Scene scene = new Scene(root);
         
